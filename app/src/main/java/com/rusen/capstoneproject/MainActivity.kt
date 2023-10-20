c91d1a0a58f2a171f2c663d1afcf4b0f0b50ffc5
rusenolcay <narincrusen@gmail.com>
2023-10-20 16:26:42 +0300

    viewbinding template eklendi ve kullanildi

--- app/src/main/java/com/rusen/capstoneproject/MainActivity.kt
+++ app/src/main/java/com/rusen/capstoneproject/MainActivity.kt
@@ -2,23 +2,25 @@ package com.rusen.capstoneproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.rusen.capstoneproject.common.viewBinding
 import com.rusen.capstoneproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+
+    private val binding by viewBinding(ActivityMainBinding::inflate)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
-        navController = navHostFragment.navController
-        val  bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         setupWithNavController(bottomNavigationView, navController)
     }
+
 }
