473898e01ad2aadba18288b1f75e0fcb45e02797
rusenolcay <narincrusen@gmail.com>
2023-10-22 16:49:01 +0300

    Firebase Auth eklendi ve uygulandi.

--- app/src/main/java/com/rusen/capstoneproject/ui/login/register/RegisterFragment.kt
+++ app/src/main/java/com/rusen/capstoneproject/ui/login/register/RegisterFragment.kt
@@ -1,9 +1,11 @@
 package com.rusen.capstoneproject.ui.login.register
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.rusen.capstoneproject.R
 import com.rusen.capstoneproject.common.viewBinding
 import com.rusen.capstoneproject.databinding.FragmentRegisterBinding
@@ -14,9 +16,36 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.ivBack.setOnClickListener{
+        binding.ivBack.setOnClickListener {
             it.findNavController().popBackStack()
+        }
+        binding.btnCreate.setOnClickListener {
+            if (binding.email.text.toString().isBlank()) {
+                Toast.makeText(
+                    context,
+                    "Lutfen gecerli bir email adresi giriniz!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (binding.password.text.toString().length < 6) {
+                Toast.makeText(
+                    context,
+                    "Lutfen en az 6 haneli bir sifre giriniz!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                register()
+            }
+        }
+    }
+
+    private fun register() {
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            binding.email.text.toString(),
+            binding.password.text.toString()
+        ).addOnSuccessListener {
+            Toast.makeText(context, "Başarılı", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(context, "Başarısız", Toast.LENGTH_SHORT).show()
         }
     }
 }
--- app/src/main/java/com/rusen/capstoneproject/ui/login/signin/SignInFragment.kt
+++ app/src/main/java/com/rusen/capstoneproject/ui/login/signin/SignInFragment.kt
@@ -2,8 +2,10 @@ package com.rusen.capstoneproject.ui.login.signin
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.rusen.capstoneproject.R
 import com.rusen.capstoneproject.common.viewBinding
 import com.rusen.capstoneproject.databinding.FragmentSignInBinding
@@ -17,6 +19,34 @@ class SignInFragment : Fragment(R.layout.fragment_sign_in) {
         binding.btnRegister.setOnClickListener {
             val action = SignInFragmentDirections.actionSignInFragmentToRegisterFragment()
             it.findNavController().navigate(action)
+        }
+        binding.btnLogin.setOnClickListener {
+            if (binding.email.text.toString().isBlank()) {
+                Toast.makeText(
+                    context,
+                    "Lutfen gecerli bir email adresi giriniz!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else if (binding.password.text.toString().length < 6) {
+                Toast.makeText(
+                    context,
+                    "Lutfen en az 6 haneli bir sifre giriniz!",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                signIn()
+            }
+        }
+    }
+
+    private fun signIn() {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            binding.email.text.toString(),
+            binding.password.text.toString()
+        ).addOnSuccessListener {
+            Toast.makeText(context, "Başarılı", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            Toast.makeText(context, "Başarısız", Toast.LENGTH_SHORT).show()
         }
     }
 }
