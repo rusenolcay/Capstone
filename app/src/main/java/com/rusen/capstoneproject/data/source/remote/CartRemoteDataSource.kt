f7f8f686fef6188e6260cf4334c263f193f58bc2
rusenolcay <narincrusen@gmail.com>
2023-11-10 00:37:37 +0300

    Cart ekranlari icin remoteDataSource eklendi

---
+++ app/src/main/java/com/rusen/capstoneproject/data/source/remote/CartRemoteDataSource.kt
@@ -0,0 +1,97 @@
+package com.rusen.capstoneproject.data.source.remote
+
+import android.util.Log
+import com.rusen.capstoneproject.CapstoneApplication
+import com.rusen.capstoneproject.data.model.ClearCartRequest
+import com.rusen.capstoneproject.data.model.GetClearCartResponse
+import com.rusen.capstoneproject.data.model.GetDeleteFromCartRequest
+import com.rusen.capstoneproject.data.model.GetDeleteFromCartResponse
+import com.rusen.capstoneproject.data.model.GetProductsCartResponse
+import com.rusen.capstoneproject.data.model.Product
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CartRemoteDataSource {
+
+    private var service = CapstoneApplication.productService
+
+    fun getCartProducts(
+        onSuccess: (List<Product>) -> Unit,
+        onFailure: (String?) -> Unit,
+        userId: String
+    ) {
+        service?.getProductsCart(userId)?.enqueue(object : Callback<GetProductsCartResponse> {
+            override fun onResponse(
+                call: Call<GetProductsCartResponse>,
+                response: Response<GetProductsCartResponse>
+            ) {
+                val result = response.body()
+                if (result?.status == 200) {
+                    onSuccess(result.products)
+                } else {
+                    onFailure(result?.message)
+                }
+            }
+
+            override fun onFailure(call: Call<GetProductsCartResponse>, t: Throwable) {
+                Log.e("GetProducts", t.message.orEmpty())
+            }
+        })
+    }
+
+    fun clearCart(
+        onSuccess: () -> Unit,
+        onFailure: (String?) -> Unit,
+        userId: String
+    ) {
+        val request = ClearCartRequest(userId)
+        service?.clearCart(request)?.enqueue(object : Callback<GetClearCartResponse> {
+            override fun onResponse(
+                call: Call<GetClearCartResponse>,
+                response: Response<GetClearCartResponse>
+            ) {
+                val result = response.body()
+                if (result?.status == 200) {
+                    onSuccess()
+                } else {
+                    onFailure(result?.message)
+                }
+            }
+
+            override fun onFailure(call: Call<GetClearCartResponse>, t: Throwable) {
+                Log.e("GetClear", t.message.orEmpty())
+            }
+        })
+    }
+
+    fun deleteProductFromCart(
+        onSuccess: (List<Product>) -> Unit,
+        onFailure: (String?) -> Unit,
+        userId: String,
+        productId: Long,
+        currentList: MutableList<Product>
+    ) {
+        val request = GetDeleteFromCartRequest(productId, userId)
+        val deleteCart = service?.deleteCart(request)
+        deleteCart?.enqueue(object : Callback<GetDeleteFromCartResponse> {
+            override fun onResponse(
+                call: Call<GetDeleteFromCartResponse>,
+                response: Response<GetDeleteFromCartResponse>
+            ) {
+                val result = response.body()
+                if (result?.status == 200) {
+                    //Basarili istek sonucunda silinmesi istenilen urunun productId'sini suanki listede filtreler ve bulunca siler.
+                    currentList.removeIf { it.id == productId }
+                    onSuccess(currentList)
+                } else {
+                    onFailure(result?.message)
+                }
+            }
+
+            override fun onFailure(call: Call<GetDeleteFromCartResponse>, t: Throwable) {
+                Log.e("GetClear", t.message.orEmpty())
+            }
+        })
+    }
+}
--- app/src/main/java/com/rusen/capstoneproject/ui/cart/CartViewModel.kt
+++ app/src/main/java/com/rusen/capstoneproject/ui/cart/CartViewModel.kt
@@ -1,97 +1,62 @@
 package com.rusen.capstoneproject.ui.cart
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.google.firebase.auth.FirebaseAuth
-import com.rusen.capstoneproject.CapstoneApplication
-import com.rusen.capstoneproject.data.model.ClearCartRequest
-import com.rusen.capstoneproject.data.model.GetClearCartResponse
-import com.rusen.capstoneproject.data.model.GetDeleteFromCartRequest
-import com.rusen.capstoneproject.data.model.GetDeleteFromCartResponse
-import com.rusen.capstoneproject.data.model.GetProductsCartResponse
 import com.rusen.capstoneproject.data.model.Product
+import com.rusen.capstoneproject.data.source.remote.CartRemoteDataSource
 import com.rusen.capstoneproject.ui.BaseViewModel
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class CartViewModel : BaseViewModel() {
 
     private val showCartProducts = MutableLiveData<List<Product>>()
     val showCartProductsEvent: LiveData<List<Product>> = showCartProducts
 
+    private val cartRemoteDataSource = CartRemoteDataSource()
 
     fun getCartProducts() {
         FirebaseAuth.getInstance().currentUser?.let { user ->
-            CapstoneApplication.productService?.getProductsCart(userId = user.uid)?.enqueue(object :
-                Callback<GetProductsCartResponse> {
-                override fun onResponse(
-                    call: Call<GetProductsCartResponse>,
-                    response: Response<GetProductsCartResponse>
-                ) {
-                    val result = response.body()
-                    if (result?.status == 200) {
-                        showCartProducts.value = result.products
-                    } else {
-                        showCartProducts.value = emptyList()
-                        showMessage.value = result?.message
-                    }
-                }
-
-                override fun onFailure(call: Call<GetProductsCartResponse>, t: Throwable) {
-                    Log.e("GetProducts", t.message.orEmpty())
-                }
-            })
+            cartRemoteDataSource.getCartProducts(
+                onSuccess = {
+                    showCartProducts.value = it
+                },
+                onFailure = {
+                    showCartProducts.value = emptyList()
+                    showMessage.value = it
+                },
+                userId = user.uid
+            )
         }
     }
 
     fun clearCart() {
         FirebaseAuth.getInstance().currentUser?.let { user ->
-            CapstoneApplication.productService?.clearCart(ClearCartRequest(userId = user.uid))
-                ?.enqueue(object : Callback<GetClearCartResponse> {
-                    override fun onResponse(
-                        call: Call<GetClearCartResponse>,
-                        response: Response<GetClearCartResponse>
-                    ) {
-                        val result = response.body()
-                        if (result?.status == 200) {
-                            showCartProducts.value = emptyList()
-                        } else {
-                            showMessage.value = result?.message
-                        }
-                    }
-
-                    override fun onFailure(call: Call<GetClearCartResponse>, t: Throwable) {
-                        Log.e("GetClear", t.message.orEmpty())
-                    }
-                })
+            cartRemoteDataSource.clearCart(
+                onSuccess = {
+                    showCartProducts.value = emptyList()
+                },
+                onFailure = {
+                    showMessage.value = it
+                },
+                userId = user.uid
+            )
         }
     }
 
     fun onProductDelete(productId: Long, currentList: MutableList<Product>) {
-        FirebaseAuth.getInstance().currentUser?.let {
-            val request = GetDeleteFromCartRequest(productId, it.uid)
-            val deleteCart = CapstoneApplication.productService?.deleteCart(request)
-            deleteCart?.enqueue(object : Callback<GetDeleteFromCartResponse> {
-                override fun onResponse(
-                    call: Call<GetDeleteFromCartResponse>,
-                    response: Response<GetDeleteFromCartResponse>
-                ) {
-                    val result = response.body()
-                    if (result?.status == 200) {
-                        //Basarili istek sonucunda silinmesi istenilen urunun productId'sini suanki listede filtreler ve bulunca siler.
-                        currentList.removeIf { it.id == productId }
-                        showCartProducts.value = currentList
-                    } else {
-                        showMessage.value = result?.message
-                    }
-                }
+        FirebaseAuth.getInstance().currentUser?.let { user ->
+            cartRemoteDataSource.deleteProductFromCart(
+                onSuccess = {
+                    showCartProducts.value = it
+                },
+                onFailure = {
+                    showMessage.value = it
+                },
+                userId = user.uid,
+                productId = productId,
+                currentList = currentList
+            )
 
-                override fun onFailure(call: Call<GetDeleteFromCartResponse>, t: Throwable) {
-                    Log.e("GetClear", t.message.orEmpty())
-                }
-            })
         }
     }
 }
